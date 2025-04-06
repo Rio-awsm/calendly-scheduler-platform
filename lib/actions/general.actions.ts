242e175b4ff6b3c9b67b2ab4b4d6a40a7f2924b2
Rio-awsm <mm.saha1982@gmail.com>
2025-04-07 02:16:05 +0530

    feat: implement CancelEvent functionality and update MeetingsPage to allow event cancellation

--- lib/actions/general.actions.ts
+++ lib/actions/general.actions.ts
@@ -227,3 +227,29 @@ const meetingLength = Number(formData.get("meetingLength"));
   })
   return redirect(`/success`);
 }
+
+export async function CancelEvent(formData: FormData) {
+  const session = await requireUser()
+
+  const userData = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id as string
+    },
+    select: {
+      grantEmail: true,
+      grantId: true,
+    }
+  })
+  if(!userData) {
+    throw new Error("User not found")
+  }
+
+  const data = await nylas.events.destroy({
+    eventId: formData.get("eventId") as string,
+    identifier: userData?.grantId as string,
+    queryParams: {
+      calendarId: userData?.grantEmail as string
+    }
+  })
+  revalidatePath("/dashboard/meetings")
+}
